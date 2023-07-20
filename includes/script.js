adf47f56ca3e573e22059fff3166c70c98203e03
mah-work <132890999+mah-work@users.noreply.github.com>
2023-07-20 11:08:14 +0200

    Update script.js

--- includes/script.js
+++ includes/script.js
@@ -1,1 +1,20 @@
+ document.addEventListener('DOMContentLoaded', function() {
+    const codeBlock = document.getElementById('code-block');
+    const keywords = ['const', 'querySelector', 'forEach'];
 
+    const text = codeBlock.textContent;
+    const regex = new RegExp(keywords.join('|'), 'gi');
+
+    codeBlock.innerHTML = text.replace(regex, match => {
+        const lowerCaseMatch = match.toLowerCase();
+        if (lowerCaseMatch === 'const') {
+            return `<span class="const">${match}</span>`;
+        } else if (lowerCaseMatch === 'queryselector') {
+            return `<span class="querySelector">${match}</span>`;
+        } else if (lowerCaseMatch === 'foreach') {
+            return `<span class="forEach">${match}</span>`;
+        } else {
+            return match;
+        }
+    });
+});
