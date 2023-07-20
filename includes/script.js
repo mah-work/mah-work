e45988da71cf5b6c42a5f6aa0a021535dfd6737f
mah-work <132890999+mah-work@users.noreply.github.com>
2023-07-20 11:25:11 +0200

    Update script.js

--- includes/script.js
+++ includes/script.js
@@ -1,4 +1,6 @@
- document.addEventListener('DOMContentLoaded', function() {
+/* Color function */ 
+
+document.addEventListener('DOMContentLoaded', function() {
     const codeBlock = document.getElementById('code-block');
     const keywords = ['const', 'querySelector', 'forEach'];
 
@@ -18,3 +20,24 @@
         }
     });
 });
+
+/* Search function */
+
+ function search() {
+      const searchInput = document.getElementById('site-search');
+      const searchTerm = searchInput.value.toLowerCase();
+
+      const collectionItems = document.querySelectorAll('.collection-item');
+
+      collectionItems.forEach(function(item) {
+          const title = item.querySelector('.title').textContent.toLowerCase();
+          const desc = item.querySelector('.desc').textContent.toLowerCase();
+          const code = item.querySelector('.code').textContent.toLowerCase();
+
+          if (title.includes(searchTerm) || desc.includes(searchTerm) || code.includes(searchTerm)) {
+              item.style.display = 'block'; // Show the matching item
+          } else {
+              item.style.display = 'none'; // Hide non-matching items
+          }
+      });
+  }
