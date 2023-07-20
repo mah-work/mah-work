8e2352d1fca4c3d913c198f0894486ddfd45b5a0
mah-work <132890999+mah-work@users.noreply.github.com>
2023-07-20 13:46:50 +0200

    Update script.js

--- includes/script.js
+++ includes/script.js
@@ -1,22 +1,25 @@
 /* Color function */ 
 document.addEventListener('DOMContentLoaded', function() {
-    const codeBlock = document.getElementById('code-block');
-    const keywords = ['const', 'querySelector', 'forEach'];
+    const codeBlocks = document.querySelectorAll('#code-block');
 
-    const text = codeBlock.textContent;
-    const regex = new RegExp(keywords.join('|'), 'gi');
+    codeBlocks.forEach(codeBlock => {
+        const keywords = ['const', 'querySelector', 'forEach'];
 
-    codeBlock.innerHTML = text.replace(regex, match => {
-        const lowerCaseMatch = match.toLowerCase();
-        if (lowerCaseMatch === 'const') {
-            return `<span class="const">${match}</span>`;
-        } else if (lowerCaseMatch === 'queryselector') {
-            return `<span class="querySelector">${match}</span>`;
-        } else if (lowerCaseMatch === 'foreach') {
-            return `<span class="forEach">${match}</span>`;
-        } else {
-            return match;
-        }
+        const text = codeBlock.textContent;
+        const regex = new RegExp(keywords.join('|'), 'gi');
+
+        codeBlock.innerHTML = text.replace(regex, match => {
+            const lowerCaseMatch = match.toLowerCase();
+            if (lowerCaseMatch === 'const') {
+                return `<span class="const">${match}</span>`;
+            } else if (lowerCaseMatch === 'queryselector') {
+                return `<span class="querySelector">${match}</span>`;
+            } else if (lowerCaseMatch === 'foreach') {
+                return `<span class="forEach">${match}</span>`;
+            } else {
+                return match;
+            }
+        });
     });
 });
 
